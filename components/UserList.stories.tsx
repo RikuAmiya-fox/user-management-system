a85ed2224b4ce03867af4feafbba857fde952bad
RikuAmiya-fox <riku.amiya@fox-hound.jp>
2025-08-26 12:45:17 +0900

    タスク1-2UserListのストーリー作成

---
+++ components/UserList.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import UserList from "./UserList";
+
+const meta: Meta<typeof UserList> = {
+  title: "Components/UserList",
+  component: UserList,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof UserList>;
+
+export const Default: Story = {
+  args: {
+    users: [
+      {
+        id: 1,
+        name: "山田 太郎",
+        email: "taro.yamada@example.com",
+        role: "管理者",
+        deleted: false,
+      },
+      {
+        id: 2,
+        name: "田中 次郎",
+        email: "jiro.tanaka@example.com",
+        role: "副管理者",
+        deleted: false,
+      },
+    ],
+  },
+};
