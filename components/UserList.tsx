91452668a267c92f6ce15e5eebe971e55f57ef37
RikuAmiya-fox <riku.amiya@fox-hound.jp>
2025-08-26 12:34:07 +0900

    タスク1-1ユーザー一覧機能の実装

---
+++ components/UserList.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import UserCard from "./UserCard";
+import { User } from "../types/User";
+
+interface UserListProps {
+  users: User[];
+}
+
+const UserList: React.FC<UserListProps> = ({ users }) => {
+  return (
+    <Box>
+      <Typography variant="h4" gutterBottom>
+        ユーザー一覧
+      </Typography>
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </Box>
+  );
+};
+
+export default UserList;
