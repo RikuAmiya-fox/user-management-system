df90e2656df5559514cd51e6199f7b6780118a29
RikuAmiya-fox <riku.amiya@fox-hound.jp>
2025-08-26 17:48:54 +0900

    追加タスクバリデーションの実装

--- components/EditUserForm.tsx
+++ components/EditUserForm.tsx
@@ -22,6 +22,18 @@ interface EditUserFormProps {
   disabled?: boolean;
 }
 
+const roleOptions = [
+  "研修生",
+  "プログラマー",
+  "SE",
+  "PL",
+  "PM",
+  "EM",
+  "事務",
+  "人事",
+  "その他",
+];
+
 // TODO: ユーザー編集フォームコンポーネントを実装する
 const EditUserForm: React.FC<EditUserFormProps> = ({
   onSuccess,
@@ -80,19 +92,51 @@ const EditUserForm: React.FC<EditUserFormProps> = ({
       <form onSubmit={handleSubmit(onSubmit)}>
         <Stack spacing={2}>
           <TextField
-            {...register("name")}
+            {...register("name", {
+              required: "名前は必須項目です",
+              minLength: {
+                value: 1,
+                message: "名前は1文字以上で入力してください",
+              },
+              maxLength: {
+                value: 20,
+                message: "名前は20文字以内で入力してください",
+              },
+              pattern: {
+                value: /^[^!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~　]+$/,
+                message: "名前に記号や全角スペースを含めることはできません",
+              },
+            })}
             placeholder="名前"
             fullWidth
+            error={!!errors.name}
+            helperText={errors.name?.message}
           ></TextField>
           <TextField
-            {...register("email")}
+            {...register("email", {
+              required: "メールアドレスは必須項目です",
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+$/,
+                message: "メールアドレスの形式が不適切です",
+              },
+            })}
             placeholder="メール"
             fullWidth
+            error={!!errors.email}
+            helperText={errors.email?.message}
           ></TextField>
           <TextField
-            {...register("role")}
+            {...register("role", {
+              required: "役職は必須項目です",
+              validate: (value) =>
+                roleOptions.includes(value) ||
+                `役職は[${roleOptions.join("、")}]のいずれかを入力してください`,
+            })}
             placeholder="役職"
             fullWidth
+            error={!!errors.role}
+            helperText={errors.role?.message}
           ></TextField>
           <Button type="submit" variant="contained" fullWidth>
             更新
