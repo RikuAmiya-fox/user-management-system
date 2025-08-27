6568dc23a4eb937d8f0e3bb78a97bafbda155583
RikuAmiya-fox <riku.amiya@fox-hound.jp>
2025-08-27 14:41:47 +0900

    2-1：カードコンポーネントの作成

---
+++ components/parts/CustomCard.tsx
@@ -0,0 +1,32 @@
+// components/parts/CustomCard.tsx
+
+import React from "react";
+import { Card, CardContent, Typography, CardActions } from "@mui/material";
+
+// TODO: インターフェースを修正
+interface CustomCardProps {
+  title: string;
+  description: React.ReactNode;
+  actions?: React.ReactNode;
+}
+
+const CustomCard: React.FC<CustomCardProps> = ({
+  title,
+  description,
+  actions,
+}) => {
+  return (
+    <Card sx={{ minWidth: 275, mb: 2 }}>
+      <CardContent>
+        {/*TODO: [titel]と[description]を表示*/}
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body1"> {description}</Typography>
+      </CardContent>
+      {actions && <CardActions>{actions}</CardActions>}
+    </Card>
+  );
+};
+
+export default CustomCard;
