d593d0787de69100504a14100575b9a804cf2743
RikuAmiya-fox <riku.amiya@fox-hound.jp>
2025-08-27 15:29:31 +0900

    2-3：UserCardの改修

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { Box } from "@mui/material";
+import { Box, Button, Link } from "@mui/material";
 import React, { useState } from "react";
-import UserCard from "./UserCard";
 import { User } from "../types/User";
+import CustomCard from "./parts/CustomCard";
+import CustomButton from "./parts/CustomButton";
+import { softDeleteUser } from "@/utils/api";
 
 interface UserListProps {
   users: User[];
@@ -12,14 +14,57 @@ interface UserListProps {
 const UserList: React.FC<UserListProps> = ({ users }) => {
   const [usersList, setUsersList] = useState<User[]>(users);
 
-  const handleDelete = (deletedId: number) => {
-    setUsersList((filterUsers) => filterUsers.filter((user) => user.id !== deletedId));
+  const handleSoftDelete = async (deleteUserID: number) => {
+    try {
+      if (confirm("本当にこのユーザーを削除しますか？")) {
+        await softDeleteUser(deleteUserID);
+        setUsersList((filterUsers) =>
+          filterUsers.filter((user) => user.id !== deleteUserID)
+        );
+      }
+    } catch (e) {
+      console.log("ユーザーの削除に失敗しました。" + e);
+    }
   };
 
   return (
     <Box>
       {usersList.map((user) => (
-        <UserCard key={user.id} user={user} onDelete={handleDelete} />
+        <CustomCard
+          key={user.id}
+          title={user.name}
+          description={
+            <>
+              メール：{user.email}
+              <br />
+              役職：{user.role}
+            </>
+          }
+          actions={
+            <>
+              <Button
+                size="small"
+                component={Link}
+                href={`/users/${user.id}/details`}
+              >
+                詳細
+              </Button>
+              <Button
+                size="small"
+                component={Link}
+                href={`/users/${user.id}/edit`}
+              >
+                編集
+              </Button>
+              <CustomButton
+                variantType="danger"
+                onClick={() => handleSoftDelete(user.id)}
+              >
+                削除
+              </CustomButton>
+            </>
+          }
+        />
       ))}
     </Box>
   );
